e058ba10410edde075193ddac2b36789d70c8586
yair-go <irigo1@gmail.com>
2022-12-12 09:07:15 +0200

    A little bit of implementation from the main menu

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -6,16 +6,30 @@ namespace DalTest // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            int choice = showMenu();
+
             DO.Student student = new DO.Student()
-                { 
-                    BirthDate = new DateTime(1990, 1, 1),
-                    PersonalId = 2000000,
-                    StartYear = 2020,
-                    Status = DO.StudentStatus.ACTIVE
-                };
+            {
+                BirthDate = new DateTime(1990, 1, 1),
+                PersonalId = 2000000,
+                StartYear = 2020,
+                Status = DO.StudentStatus.ACTIVE
+            };
             Dal.Student dalStudent = new Dal.Student();
-            dalStudent.Add(student);
-            Console.WriteLine("Hello World!");
+            int newStudentID = dalStudent.Add(student);
+            Console.WriteLine($"New Student ID : {newStudentID}" );
+            Console.WriteLine(student);
+        }
+
+        private static int showMenu()
+        {
+            Console.WriteLine("Please enter \n" +
+                        "0 - exit \n" +
+                        "1 - Students \n" +
+                        "2 - Coureses \n" +
+                        "3 - Grades");
+            int choice = int.Parse(Console.ReadLine());
+            return choice;
         }
     }
 }
