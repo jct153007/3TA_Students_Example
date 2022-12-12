bb58af8baae1dcd1d7a90d70f1b1317b76898d81
yair-go <irigo1@gmail.com>
2022-12-12 12:09:02 +0200

    BO Entities

---
+++ BL/BO/Exceptions.cs
@@ -0,0 +1,26 @@
+﻿namespace BO;
+
+[Serializable]
+public class BadIdException : Exception
+{
+    public BadIdException(string message) : base(message) { }
+}
+
+[Serializable]
+public class NotEnoughException : Exception
+{
+    public NotEnoughException(string message) : base(message) { }
+}
+
+[Serializable]
+public class InternalProblemException : Exception
+{
+    public InternalProblemException(string message) : base(message) { }
+    public InternalProblemException(string message, Exception inner) : base(message, inner) { }
+}
+
+[Serializable]
+public class EntityDoesNotExistException : Exception
+{
+    public EntityDoesNotExistException(string message, Exception inner) : base(message, inner) { }
+}
